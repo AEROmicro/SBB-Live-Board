8def4c719952e42e88aa7655c4afd0c85b47ea29
ÆROxol <callumfchang@gmail.com>
2026-03-14 12:05:05 -0400

    Add style.css and script.js to cache on install

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('sbb-v1').then((cache) => cache.addAll(['index.html', 'manifest.json']))
+    caches.open('sbb-v1').then((cache) => cache.addAll(['index.html', 'style.css', 'script.js', 'manifest.json']))
   );
 });
 
